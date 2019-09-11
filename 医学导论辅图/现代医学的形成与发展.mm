b163a4ad41718f5f702644a31a820aa9292e6793
Sam Yan <896298116@qq.com>
2019-09-11 15:37:12 +0800

    完成了20世纪医学成就部分学习
    完成了20世纪医学成就部分学习

---
+++ 医学导论辅图/现代医学的形成与发展.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1568169633817" ID="ID_141577048" MODIFIED="1568170607592" TEXT="&#x73b0;&#x4ee3;&#x533b;&#x5b66;&#x7684;&#x5f62;&#x6210;&#x4e0e;&#x53d1;&#x5c55;">
+<node CREATED="1568169944683" ID="ID_710472927" MODIFIED="1568169957472" POSITION="right" TEXT="&#x836f;&#x7269;&#x5b66;&#x4e0e;&#x6cbb;&#x7597;&#x5b66;&#x7684;&#x98de;&#x8dc3;"/>
+<node CREATED="1568169961782" ID="ID_953165417" MODIFIED="1568169973379" POSITION="left" TEXT="&#x5206;&#x5b50;&#x751f;&#x7269;&#x5b66;&#x7684;&#x53d1;&#x5c55;"/>
+<node CREATED="1568169978082" ID="ID_921253552" MODIFIED="1568169986912" POSITION="right" TEXT="&#x514d;&#x75ab;&#x5b66;&#x7684;&#x53d1;&#x5c55;"/>
+<node CREATED="1568169990471" ID="ID_357889367" MODIFIED="1568169997831" POSITION="left" TEXT="&#x533b;&#x5b66;&#x9057;&#x4f20;&#x5b66;&#x7684;&#x53d1;&#x5c55;"/>
+<node CREATED="1568170004021" ID="ID_755798794" MODIFIED="1568170012440" POSITION="right" TEXT="&#x5668;&#x5b98;&#x79fb;&#x690d;&#x4e0e;&#x4eba;&#x9020;&#x5668;&#x5b98;&#x6280;&#x672f;"/>
+<node CREATED="1568170017070" ID="ID_716546997" MODIFIED="1568170021470" POSITION="left" TEXT="&#x533b;&#x5b66;&#x6a21;&#x5f0f;&#x7684;&#x8f6c;&#x53d8;"/>
+<node CREATED="1568170032829" ID="ID_1795567180" MODIFIED="1568170053298" POSITION="right" TEXT="&#x4f20;&#x67d3;&#x75c5;&#x9632;&#x6cbb;&#x8fdb;&#x5c55;"/>
+<node CREATED="1568170065047" ID="ID_727553509" MODIFIED="1568170070827" POSITION="left" TEXT="&#x533b;&#x5b66;&#x5f71;&#x50cf;&#x5b66;&#x7684;&#x53d1;&#x5c55;"/>
+</node>
+</map>
